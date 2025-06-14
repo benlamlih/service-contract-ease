cf8f63bb6b831a7cfd84104548dca59eb7f7de12
Mohammed Benlamlih <45820673+benlamlih@users.noreply.github.com>
2025-06-14 11:49:10 +0200

    improve the openapi doc

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -27,20 +27,13 @@ func (s *Server) RegisterRoutes(tp trace.TracerProvider) http.Handler {
 		AllowCredentials: true,
 	}))
 
-	r.GET("/", s.HelloWorldHandler)
-
-	r.GET("/health", s.healthHandler)
-
-	r.GET("/reference", s.apiReferenceHandler)
+	api := r.Group("/api")
+	{
+		api.GET("/health", s.healthHandler)
+		api.GET("/docs", s.apiReferenceHandler)
+	}
 
 	return r
-}
-
-func (s *Server) HelloWorldHandler(c *gin.Context) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	c.JSON(http.StatusOK, resp)
 }
 
 func (s *Server) healthHandler(c *gin.Context) {
